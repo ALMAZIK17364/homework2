17fbfd26b34d88f4504f9c6df128946a2e6bdc78
MrAlM <81728811+ALMAZIK17364@users.noreply.github.com>
2024-07-08 17:56:10 +0800

    Add files via upload
    Практическое задание по теме "Переменные."

---
+++ homework2.py
@@ -0,0 +1,5 @@
+HW_Count = 12
+hours_count = 1.5
+course_name = "Python"
+time_for_task = hours_count / HW_Count
+print("Курс:",course_name+", всего задач:",HW_Count,"| затрачено часов:",hours_count,"| среднее время выполнения:",time_for_task,"часа.")
